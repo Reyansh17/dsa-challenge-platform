344d9063197ce660f496577b8e59a6b7f710cd43
Reyansh17 <113831002+Reyansh17@users.noreply.github.com>
2025-02-10 09:50:19 +0530

    fixed finally

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,12 +4,17 @@ declare module 'next-auth' {
   interface Session {
     user: {
       id: string;
-      email: string;
-      name: string;
-      role: string;
-      avatar: string;
-      avatarStyle: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
       role?: string;
+      totalProblemsSolved: number;
+      easySolved: number;
+      mediumSolved: number;
+      hardSolved: number;
+      isAdminToday: boolean;
+      isEligibleForAdmin: boolean;
+      lastAdminDate?: Date;
     }
   }
 
